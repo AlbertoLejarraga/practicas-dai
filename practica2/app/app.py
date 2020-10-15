f908da231d763888a6d45f75b19b787a65ddffb9
Alberto <albertolej@hotmail.com>
2020-10-15 13:34:24 +0200

    se empieza primer ejercicio y se suben ficheros necesarios de p1

--- practica2/app/app.py
+++ practica2/app/app.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from app.ejP1 import adivinaElNumero as ej1p1
+from app.ejP1 import ordenacionMatrices as ej2p1
+from app.ejP1 import cribaEratostenes as ej3p1
+from app.ejP1 import fibonacciFichero as ej4p1
+from app.ejP1 import cadenasCorchetes as ej5p1
+from app.ejP1 import expresionesRegulares as ej6p1
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/ejercicio2/<string:matriz>')
+def ejercicio2(matriz):
+    return list(matriz)
