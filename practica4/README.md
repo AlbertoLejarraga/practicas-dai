cf0533c9ea7db48266aaf817b53fd16b577f44f6
= <alejarragar@correo.ugr.es>
2020-11-17 18:46:47 +0100

    Se genera la estructura de la practica 4

---
+++ practica4/README.md
@@ -0,0 +1,2 @@
+# Práctica 3 de la asignatura
+Práctica basada en el uso de la arquitectura MVC para montar la estructura de una página web utilizando el framework css "bootstrap" y el motor de plantillas de Flask, Jinja. Incluye también funcionalidad de login y una interfaz web para los ejercicios de la práctica 1.
---
+++ practica4/app/model.py
@@ -0,0 +1,29 @@
+from pickleshare import *
+import hashlib
+
+db = PickleShareDB("dbUsuarios")
+def existeUsuario(nick):
+    return nick in db
+def usuarioNuevo(nick, nombre, apellidos, mail, pwd):
+    db[nick] = {"nombre":nombre, "apellidos":apellidos, "mail": mail, "pwd":hashlib.sha256(pwd.encode('utf-8')).hexdigest()}
+def comprobarPwd(nick, pwd):
+    return db[nick]["pwd"] == hashlib.sha256(pwd.encode('utf-8')).hexdigest()
+def obtenerDatosUsuario(nick):
+    return db[nick]
+def modificarUsuario(nick, nombre, apellidos, mail):
+    db[nick]["nombre"] = nombre
+    db[nick]["apellidos"] = apellidos
+    db[nick]["mail"] = mail
+    db[nick] = db[nick]
+
+def modificarPwd(nick, pwd):
+    db[nick]["pwd"] =  hashlib.sha256(pwd.encode('utf-8')).hexdigest()
+    db[nick] = db[nick]
+def modificarListaVisitadas(nick, lista):
+    db[nick]["listaVisitadas"] = lista[:5]
+    db[nick] = db[nick]
+def obtenerListaWebsUsuario(nick):
+    if "listaVisitadas" in db[nick]:
+        return db[nick]["listaVisitadas"]
+    else:
+        return []
